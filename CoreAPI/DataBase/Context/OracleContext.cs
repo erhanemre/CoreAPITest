a0f26904a8f7b194a79b61be30c23061e3e74aac
Erhan Emre DİĞA <e_diga@organikkimya.com>
2020-10-25 19:04:41 +0300

    Added

--- CoreAPI/DataBase/Context/OracleContext.cs
+++ CoreAPI/DataBase/Context/OracleContext.cs
@@ -45,7 +45,7 @@ namespace CoreAPI.DataBase.Context
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 //optionsBuilder.UseOracle("Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.0.0.4)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=OKDB)));Persist Security Info=True;User Id=AVLOWN;Password=admokdb*1881!;", options => options.UseOracleSQLCompatibility("11"));
-                optionsBuilder.UseOracle("Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.0.0.4)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=OKDB)));Persist Security Info=True;User Id=AVLOWN;Password=admokdb*1881!;", options => options.UseOracleSQLCompatibility("11"));
+                optionsBuilder.UseOracle("Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=HOST)(PORT=PORT))(CONNECT_DATA=(SERVICE_NAME=DB)));Persist Security Info=True;User Id=DBUSER;Password=DBPASS;", options => options.UseOracleSQLCompatibility("11"));
                 //var lf = new LoggerFactory();
                 //lf.AddProvider(new MyLoggerProvider());
                 //optionsBuilder.UseLoggerFactory(lf);
